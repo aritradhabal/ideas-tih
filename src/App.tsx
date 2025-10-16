77a667e819d5be536356444fac97b3ec4abc36ee
aritradhabal <aritradhabal36@gmail.com>
2025-10-16 18:27:56 +0530

    Refactor routing structure by creating RouteHandler component; integrate React Query for data fetching and remove Navbar component. Update db.json with additional product entries and add @tanstack/react-query dependency in package.json and pnpm-lock.yaml.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
-import { Navigate, Route, Routes } from "react-router-dom";
-import ProductPage from "./pages/ProductPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
-import ProfilePage from "./pages/ProfilePage";
-import SettingsPage from "./pages/SettingsPage";
-import CartPage from "./pages/CartPage";
-import Navbar from "./pages/Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import RouteHandler from "./RouteHandler";
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
-      <Routes>
-        <Route
-          path="/home"
-          element={<Navigate to="/home/products" replace />}
-        />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/home" element={<Navbar />}>
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="settings" element={<SettingsPage />} />
-          <Route path="products" element={<ProductPage />} />
-          <Route path="products/:id" element={<ProductDetailsPage />} />
-          <Route path="cart" element={<CartPage />} />
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <RouteHandler />
+      </QueryClientProvider>
     </>
   );
 }
---
+++ src/RouteHandler.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import HomePage from "@/pages/HomePage";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductPage from "@/pages/ProductPage";
+import ProductDetailsPage from "@/pages/ProductDetailsPage";
+import ProfilePage from "@/pages/ProfilePage";
+import SettingsPage from "@/pages/SettingsPage";
+import CartPage from "@/pages/CartPage";
+import Navbar from "@/pages/Navbar";
+
+const RouteHandler = () => {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/home"
+          element={<Navigate to="/home/products" replace />}
+        />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navbar />}>
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="settings" element={<SettingsPage />} />
+          <Route path="products" element={<ProductPage />} />
+          <Route path="products/:id" element={<ProductDetailsPage />} />
+          <Route path="cart" element={<CartPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default RouteHandler;
