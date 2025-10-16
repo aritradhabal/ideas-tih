6decac65b631b5ef8a0dd89e4e8201d60d9510d0
aritradhabal <aritradhabal36@gmail.com>
2025-10-16 13:08:10 +0530

    Initialize React + TypeScript + Vite project with essential configurations and components. Added .gitignore, ESLint configuration, TypeScript settings, and basic project structure including App component, styling, and utility functions.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="outline">Hi</Button>
+    </>
+  );
+}
+
+export default App;
